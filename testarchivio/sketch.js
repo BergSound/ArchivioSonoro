05d2d914528cdd28d80aeec0ff76caff6193f638
BergSound <87539394+BergSound@users.noreply.github.com>
2021-07-16 20:21:55 +0200

    v2

--- testarchivio/sketch.js
+++ testarchivio/sketch.js
@@ -7,6 +7,26 @@ let termy;
 let r=10;
 let mySound;
 
+class luogo{
+	constructor(x, y){
+		this.x = x
+		this.y = y
+	}
+	get wd() {
+		return this.x
+	}
+	get ht() {
+		return this.y
+	}
+}
+
+const terminio = new luogo(40.83874, 14.9383);
+const accellica = new luogo(40.777, 15,006);
+let places = [terminio, accellica]
+
+let sounds = []
+let snn = ['ambientone_laghi.wav', '01 San Biagio.wav']
+
 // setup
 const options = {
   lat: 40.777,
@@ -15,50 +35,44 @@ const options = {
   style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
 }
 
+function preload() {
+  soundFormats('mp3', 'wav');
+	for(const i of snn){
+		sounds.push(loadSound(i))
+	}
+}
+
 function setup(){
  
-  canvas = createCanvas(800,400); 
+  canvas = createCanvas(800,400) 
 
-  myMap = mappa.tileMap(options); 
-  myMap.overlay(canvas);
-  
+  myMap = mappa.tileMap(options)
+  myMap.overlay(canvas)
+}
+
+function getpx(x, y){
+	return myMap.latLngToPixel(x, y);
 }
 
 function draw(){
   clear();
-  
-  const terminio = myMap.latLngToPixel(40.83874, 14.9383); 
-  
+	
 //-----disegno cerchi per le coordinate
   fill(255, 0, 0)
-  ellipse(terminio.x, terminio.y, 20, 20);
-  termx=terminio.x;
-  termy=terminio.y;
+	for (const i of places){
+		ellipse((getpx(i.wd, i.ht)).x, (getpx(i.wd, i.ht)).y, d_cerchio, d_cerchio)
+	}
   if (mouseIsPressed) {
     ellipse(mouseX, mouseY, 20, 20);
+	  for (const i of places){
+		let d = dist(mouseX, mouseY, (getpx(i.wd, i.ht)).x, (getpx(i.wd, i.ht)).y)
+		if (d < r) {
+			let n = places.indexOf(i)
+			sounds[n].play()
+  		}	
+	}
 }
 }
 
-function mousePressed() {
-  let d = dist(mouseX, mouseY, termx, termy);
-  if (d < r) {
-    mySound.play();
-  }
-}
-
-function preload() {
-  soundFormats('mp3', 'wav');
-  mySound=loadSound('ex.wav');
-}
 
 
-
-/*function draw(){
-  clear();
-  
-  const terminio = myMap.latLngToPixel(40.83874, 14.9383); 
-  
-//-----disegno cerchi per le coordinate-----//
-  fill(255, 0, 0)
-  ellipse(terminio.x, terminio.y, d_cerchio, d_cerchio);
-}*/
